38cd3b9cbb54fa42cd5b98a08e2b8d0629806717
Sheriffdeen Muritala Adegoke <muritalasheriffdeenadegoke@gmail.com>
2024-06-19 17:11:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Add event listener to navigation links
+navLinks = document.querySelectorAll('nav a');
+navLinks.forEach(link => {
+	link.addEventListener('click', event => {
+		event.preventDefault();
+		const target = event.target.getAttribute('href');
+		document.querySelector(target).scrollIntoView();
+	});
+});
